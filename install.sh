b82a9f02edbae9a0f3dd116291fba094fb81b83b
Moncii <yoshiroberts@proton.me>
2024-09-02 16:38:16 -0400

    Added gkt theme and icons.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 sudo pacman -S unzip fzf exa bat stow \
 	pamixer playerctl pavucontrol udiskie trash-cli lf \
 	chafa foot waybar calcurse swaybg nwg-look python-httplib2 \
-	firefox docker gdu bottom \
+	thermald thunar firefox docker gdu bottom \
 	ttf-jetbrains-mono-nerd noto-fonts-emoji
 
 sudo pacman -S --needed base-devel
@@ -13,6 +13,8 @@ cd ../ && rm -rf paru
 
 # AUR
 paru -S tofi
+
+systemctl enable --now thermald.service
 
 systemctl enable --now docker.service
 sudo usermod -aG docker $USER
