30df6f029451e5b2d2e36a5354de2a3ee1cc8e84
yoshi-roberts <yoshiroberts@proton.me>
2025-01-18 17:46:02 -0500

    More minimal.

--- install.sh
+++ install.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/sh
-sudo pacman -S unzip fzf exa bat stow \
+sudo pacman -S zip unzip fzf exa bat stow \
 	pamixer playerctl pavucontrol udiskie trash-cli lf \
-	chafa foot waybar calcurse swaybg nwg-look python-httplib2 \
-	thermald thunar firefox docker gdu bottom \
-	ttf-jetbrains-mono-nerd noto-fonts-emoji
+	chafa foot waybar hyprpaper calcurse python-httplib2 \
+	thermald firefox docker gdu bottom audacious \
+	thunar thunar-archive-plugin engrampa \
+	adw-gtk-theme ttf-jetbrains-mono-nerd noto-fonts noto-fonts-cjk noto-fonts-emoji \
+	hyprlock hypridle rofi-wayland nwg-look
 
-sudo pacman -S --needed base-devel
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
-cd ../ && rm -rf paru
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay && makepkg -si
 
 # AUR
-paru -S tofi
+yay -S swayosd-git hyprshot qimgv
 
+# Start Services.
+systemctl enable --now swayosd-libinput-backend.service
 systemctl enable --now thermald.service
 
 systemctl enable --now docker.service
 sudo usermod -aG docker $USER
 
 # Wallpapers
-git clone https://codeberg.org/Moncii/wallpapers.git ~/Pictures/wallpapers
+git clone https://codeberg.org/moncii/wallpapers.git ~/Pictures/wallpapers
+
+# Exit message.
+echo 
+echo "==== Finished ===="
+echo "You should reboot."
+echo
